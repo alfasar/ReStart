6aa75838d5948e6228e60ae9a73c63baa87eb6cc
alfasar <amettyristhirhara@gmail.com>
2024-06-08 16:24:59 +0300

    Initial commit

--- app/src/main/java/com/alfasar/restart/MainActivity.kt
+++ app/src/main/java/com/alfasar/restart/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.alfasar.restart
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
